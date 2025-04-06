1501c107aa6d5387691eb98864620a97e39bd1a5
Eriberto Lima <ter.eriberto.lima@sada.com.br>
2025-04-06 17:39:31 -0300

    Add custom exceptions and error handling middleware
    Implemented `BadRequestException` and `NotFoundException` for specific error scenarios. Introduced `ExceptionHandlingMiddleware` to log errors and return structured JSON responses with appropriate status codes. Created `ErrorResponse` class for standardized error formatting. Updated project to target .NET 8.0 and added necessary package references.

---
+++ TaskManager.CrossCutting/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TaskManager.CrossCutting.Exceptions;
+using TaskManager.CrossCutting.Responses;
+
+
+namespace TaskManager.CrossCutting.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exceção não tratada.");
+
+                context.Response.ContentType = "application/json";
+
+                var response = new ErrorResponse();
+                var statusCode = (int)HttpStatusCode.InternalServerError;
+
+                if (ex is NotFoundException)
+                {
+                    statusCode = (int)HttpStatusCode.NotFound;
+                    response.Message = "Recurso não encontrado.";
+                }
+                else if (ex is BadRequestException)
+                {
+                    statusCode = (int)HttpStatusCode.BadRequest;
+                    response.Message = "Requisição inválida.";
+                }
+                else
+                {
+                    statusCode = (int)HttpStatusCode.InternalServerError;
+                    response.Message = "Erro interno do servidor.";
+                }
+
+
+                response.StatusCode = statusCode;
+                response.Timestamp = DateTime.UtcNow;
+                response.Detail = ex.Message;
+
+                context.Response.StatusCode = statusCode;
+                var json = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
