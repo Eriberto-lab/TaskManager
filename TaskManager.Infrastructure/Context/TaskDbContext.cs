4ff03b7a6bde81b0cb6f757ee5e1eb5059bcd759
Eriberto Lima <ter.eriberto.lima@sada.com.br>
2025-04-06 17:39:11 -0300

    Add TaskDbContext and TaskRepository for task management
    Implemented TaskDbContext for managing task items with Entity Framework.
    Created TaskRepository to handle CRUD operations asynchronously.
    Updated project file to reference Microsoft.EntityFrameworkCore 9.0.3
    and set target framework to .NET 8.0 with nullable and implicit usings.

---
+++ TaskManager.Infrastructure/Context/TaskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Context
+{
+    public class TaskDbContext : DbContext
+
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    }
+}
---
+++ TaskManager.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Interfaces;
+using TaskManager.Infrastructure.Context;
+
+namespace TaskManager.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+
+        private readonly TaskDbContext _context;
+        public TaskRepository(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TaskItem task)
+        {
+               _context.Tasks.Add(task);
+             await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var task = await GetByIdAsync(id);
+            if (task is not null)
+            {
+                _context.Tasks.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            var tasks = await _context.Tasks.AsNoTracking().ToListAsync();
+            return tasks;
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id)
+        {
+           var task = await _context.Tasks.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+            return task;
+        }
+
+        public async Task UpdateAsync(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
